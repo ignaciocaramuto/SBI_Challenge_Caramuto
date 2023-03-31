7f3625d6a0e1c9b2dda2cc1e85f8db7f2c86040b
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-03-30 21:41:47 -0300

    Handler de GetDummyData + GetDummyDataQuery

---
+++ SBI_Challenge_Caramuto/Handlers/GetDummyDataHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+using SBI_Challenge_Caramuto.Queries;
+using SBI_Challenge_Caramuto.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System;
+
+namespace SBI_Challenge_Caramuto.Handlers
+{
+    public class GetDummyDataHandler : IRequestHandler<GetDummyDataQuery, List<ServerPost>>
+    {
+        private HttpClient _client;
+        private string _url;
+
+        public GetDummyDataHandler()
+        {
+            _client = new HttpClient();
+            _url = "https://jsonplaceholder.typicode.com/posts";
+        }
+        public async Task<List<ServerPost>> Handle(GetDummyDataQuery request, CancellationToken cancellationToken)
+        {
+            List<ServerPost> serverPostList;
+            try
+            {
+                var response = await _client.GetAsync(_url);
+                var content = await response.Content.ReadAsStringAsync();
+                serverPostList = JsonSerializer.Deserialize<List<ServerPost>>(content);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return serverPostList;
+        }
+    }
+}
---
+++ SBI_Challenge_Caramuto/Queries/GetDummyDataQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using SBI_Challenge_Caramuto.Model;
+using System.Collections.Generic;
+
+namespace SBI_Challenge_Caramuto.Queries
+{
+    public class GetDummyDataQuery : IRequest<List<ServerPost>> { }
+}
