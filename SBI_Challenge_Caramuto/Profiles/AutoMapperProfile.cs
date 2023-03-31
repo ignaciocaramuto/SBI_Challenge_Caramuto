27aa652923192e7426e57f2aeedfd0b691643246
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-03-30 21:40:01 -0300

    configuracion del perfil de AutoMapper

---
+++ SBI_Challenge_Caramuto/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SBI_Challenge_Caramuto.DTO;
+using SBI_Challenge_Caramuto.Model;
+
+namespace SBI_Challenge_Caramuto.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ServerPost, Salida>()
+                .ForMember(dest => dest.Titulo, opt => opt.MapFrom(src => src.title));
+        }
+    }
+}
