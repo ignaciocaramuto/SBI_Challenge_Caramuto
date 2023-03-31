c799cf5f71373a6a94e72dbaaa924cfb9309d2ee
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-03-30 21:40:48 -0300

    controlador con método GetDummyData para consultar a la api de ejemplo

---
+++ SBI_Challenge_Caramuto/Controllers/ChallengeController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SBI_Challenge_Caramuto.DTO;
+using SBI_Challenge_Caramuto.Queries;
+using System.Threading.Tasks;
+
+namespace SBI_Challenge_Caramuto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChallengeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ChallengeController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet("GetDummyData")]
+        public async Task<IActionResult> GetDummyData(int id)
+        {
+            var dummyDataquery = new GetDummyDataQuery();
+            var dummyDataresponse = await _mediator.Send(dummyDataquery);
+            var dummyDataelement = dummyDataresponse.Find(item => item.id == id);
+            Salida salida = _mapper.Map<Salida>(dummyDataelement);
+            return Ok(salida);
+        }
+    }
+}
